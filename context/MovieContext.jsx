16eb96dcedca768e4c71e5f58553c2f130aad246
Adam Romero <adamromero89@gmail.com>
2023-07-25 23:55:11 -0700

    Trying last config change again

--- context/MovieContext.jsx
+++ context/MovieContext.jsx
@@ -17,9 +17,7 @@ export const MovieProvider = ({ children }) => {
    });
 
    const getMovieVotes = async () => {
-      const response = await fetch(
-         `${process.env.NEXT_PUBLIC_API_URL}/api/movies`
-      );
+      const response = await fetch(`/api/movies`);
       const movies = await response.json();
       setMoviesList(movies);
    };
@@ -45,10 +43,7 @@ export const MovieProvider = ({ children }) => {
       };
 
       try {
-         const response = await fetch(
-            `${process.env.NEXT_PUBLIC_API_URL}/api/movies`,
-            config
-         );
+         const response = await fetch(`/api/movies`, config);
          const data = await response.json();
          setMoviesList((movies) => [...movies, data]);
          return data;
@@ -77,10 +72,7 @@ export const MovieProvider = ({ children }) => {
       });
 
       try {
-         const response = await fetch(
-            `${process.env.NEXT_PUBLIC_API_URL}/api/movies/${movieId}`,
-            config
-         );
+         const response = await fetch(`/api/movies/${movieId}`, config);
          const data = await response.json();
          setMoviesList(updatedMoviesList);
          return data;
@@ -108,10 +100,7 @@ export const MovieProvider = ({ children }) => {
       });
 
       try {
-         const response = await fetch(
-            `${process.env.NEXT_PUBLIC_API_URL}/api/movies/${movieId}`,
-            config
-         );
+         const response = await fetch(`/api/movies/${movieId}`, config);
          const data = await response.json();
          setMoviesList(updatedMoviesList);
          return data;
@@ -140,10 +129,7 @@ export const MovieProvider = ({ children }) => {
       });
 
       try {
-         const response = await fetch(
-            `${process.env.NEXT_PUBLIC_API_URL}/api/movies/${movieId}`,
-            config
-         );
+         const response = await fetch(`/api/movies/${movieId}`, config);
          const data = await response.json();
          setMoviesList(updatedMoviesList);
          return data;
