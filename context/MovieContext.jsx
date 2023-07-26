e7c3ddcc3a49675ec5407327dc81d9cff177be4c
Adam Romero <adamromero89@gmail.com>
2023-07-25 23:22:33 -0700

    Add http proxy for localhost

--- context/MovieContext.jsx
+++ context/MovieContext.jsx
@@ -17,7 +17,7 @@ export const MovieProvider = ({ children }) => {
    });
 
    const getMovieVotes = async () => {
-      const response = await fetch("http://localhost:3000/api/movies");
+      const response = await fetch("/api/movies");
       const movies = await response.json();
       setMoviesList(movies);
    };
@@ -43,10 +43,7 @@ export const MovieProvider = ({ children }) => {
       };
 
       try {
-         const response = await fetch(
-            "http://localhost:3000/api/movies",
-            config
-         );
+         const response = await fetch("/api/movies", config);
          const data = await response.json();
          setMoviesList((movies) => [...movies, data]);
          return data;
@@ -75,10 +72,7 @@ export const MovieProvider = ({ children }) => {
       });
 
       try {
-         const response = await fetch(
-            `http://localhost:3000/api/movies/${movieId}`,
-            config
-         );
+         const response = await fetch(`/api/movies/${movieId}`, config);
          const data = await response.json();
          setMoviesList(updatedMoviesList);
          return data;
@@ -106,10 +100,7 @@ export const MovieProvider = ({ children }) => {
       });
 
       try {
-         const response = await fetch(
-            `http://localhost:3000/api/movies/${movieId}`,
-            config
-         );
+         const response = await fetch(`/api/movies/${movieId}`, config);
          const data = await response.json();
          setMoviesList(updatedMoviesList);
          return data;
@@ -138,10 +129,7 @@ export const MovieProvider = ({ children }) => {
       });
 
       try {
-         const response = await fetch(
-            `http://localhost:3000/api/movies/${movieId}`,
-            config
-         );
+         const response = await fetch(`/api/movies/${movieId}`, config);
          const data = await response.json();
          setMoviesList(updatedMoviesList);
          return data;
--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
    images: {
       domains: ["c10.patreonusercontent.com"],
    },
+   async rewrites() {
+      return [
+         {
+            source: "/api/:path*",
+            destination: `${process.env.API_URL}/:path*`,
+         },
+      ];
+   },
 };
 
 module.exports = nextConfig;
