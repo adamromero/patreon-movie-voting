ac070cca7c82efde3ff48cb9f64a10d10898acd8
Adam Romero <adamromero89@gmail.com>
2023-08-02 10:40:28 -0700

    Restyle pagination numbers

--- app/components/Pagination.jsx
+++ app/components/Pagination.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+const Pagination = ({ postsPerPage, totalPosts, currentPage, paginate }) => {
    const pageNumbers = [];
    for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
       pageNumbers.push(i);
@@ -13,7 +13,9 @@ const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
                <li key={number}>
                   <button
                      onClick={() => paginate(number)}
-                     className="bg-black px-[5px]"
+                     className={`${
+                        currentPage === number ? "bg-[#620062]" : ""
+                     } px-[8px] border-[1px] border-white rounded-[3px] hover:bg-[#620062] focus:bg-[#620062]`}
                   >
                      {number}
                   </button>
