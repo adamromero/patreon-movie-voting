919eb77ec89fdd1771db3b05127fee071f20151c
Adam Romero <adamromero89@gmail.com>
2023-08-20 13:49:34 -0700

    Comment out callback functions on next auth route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -28,72 +28,72 @@ export const nextAuthOptions = {
       signIn: "/unauthorized",
    },
    secret: process.env.NEXTAUTH_SECRET,
-   callbacks: {
-      async jwt({ token, account, profile }) {
-         if (account) {
-            token.accessToken = account.access_token;
-            token.id = profile.id;
-         }
-         return token;
-      },
-      async session({ token, session }) {
-         const response = await axios.get(process.env.PATREON_PROFILE_URL, {
-            headers: {
-               Authorization: `Bearer ${token.accessToken}`,
-            },
-         });
+   // callbacks: {
+   //    async jwt({ token, account, profile }) {
+   //       if (account) {
+   //          token.accessToken = account.access_token;
+   //          token.id = profile.id;
+   //       }
+   //       return token;
+   //    },
+   //    async session({ token, session }) {
+   //       const response = await axios.get(process.env.PATREON_PROFILE_URL, {
+   //          headers: {
+   //             Authorization: `Bearer ${token.accessToken}`,
+   //          },
+   //       });
 
-         const user = response.data;
-         const { id } = user.data;
-         const { first_name, full_name, image_url } = user.data.attributes;
-         const isCreator = id === process.env.CREATOR_ID;
+   //       const user = response.data;
+   //       const { id } = user.data;
+   //       const { first_name, full_name, image_url } = user.data.attributes;
+   //       const isCreator = id === process.env.CREATOR_ID;
 
-         if (token) {
-            session.user.id = id;
-            session.user.firstName = first_name;
-            session.user.name = full_name;
-            session.user.image = image_url;
-            session.user.isCreator = isCreator;
-         }
+   //       if (token) {
+   //          session.user.id = id;
+   //          session.user.firstName = first_name;
+   //          session.user.name = full_name;
+   //          session.user.image = image_url;
+   //          session.user.isCreator = isCreator;
+   //       }
 
-         return session;
-      },
-      async signIn({ account, profile }) {
-         const response = await axios.get(process.env.PATREON_PROFILE_URL, {
-            headers: {
-               Authorization: `Bearer ${account.access_token}`,
-            },
-         });
+   //       return session;
+   //    },
+   //    async signIn({ account, profile }) {
+   //       const response = await axios.get(process.env.PATREON_PROFILE_URL, {
+   //          headers: {
+   //             Authorization: `Bearer ${account.access_token}`,
+   //          },
+   //       });
 
-         let isPledged = false;
-         const pledge = response?.included?.find(
-            (item) =>
-               item.type === "pledge" &&
-               item.relationships.creator.data.id === process.env.CREATOR_ID
-         );
+   //       let isPledged = false;
+   //       const pledge = response?.included?.find(
+   //          (item) =>
+   //             item.type === "pledge" &&
+   //             item.relationships.creator.data.id === process.env.CREATOR_ID
+   //       );
 
-         if (pledge) {
-            const {
-               attributes: { status },
-            } = pledge;
-            isPledged = status === "valid";
-         }
+   //       if (pledge) {
+   //          const {
+   //             attributes: { status },
+   //          } = pledge;
+   //          isPledged = status === "valid";
+   //       }
 
-         const { id } = profile.data;
-         const isCreator = id === process.env.CREATOR_ID;
-         const isDev = id === process.env.DEV_ID;
-         const isAllowedToSignIn = isPledged || isCreator || isDev;
+   //       const { id } = profile.data;
+   //       const isCreator = id === process.env.CREATOR_ID;
+   //       const isDev = id === process.env.DEV_ID;
+   //       const isAllowedToSignIn = isPledged || isCreator || isDev;
 
-         if (isAllowedToSignIn) {
-            return true;
-         } else {
-            return "/unauthorized";
-         }
-      },
-      async redirect({ url, baseUrl }) {
-         return baseUrl;
-      },
-   },
+   //       if (isAllowedToSignIn) {
+   //          return true;
+   //       } else {
+   //          return "/unauthorized";
+   //       }
+   //    },
+   //    async redirect({ url, baseUrl }) {
+   //       return baseUrl;
+   //    },
+   // },
 };
 
 const handler = NextAuth(nextAuthOptions);
