aee6eeeccd9d83af8d57364534f34c8c5b3c848a
Adam Romero <adamromero89@gmail.com>
2023-09-04 09:35:42 -0700

    Put site out of service for now

---
+++ app/maintenance/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MaintenancePage = () => {
+   return (
+      <div className="flex justify-center items-center h-[100vh] text-[20px]">
+         Temporarily down for maintenance. Thanks for your patience.
+      </div>
+   );
+};
+
+export default MaintenancePage;
--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import Header from "./components/Header";
 import { getCurrentUser } from "@/lib/session";
 import VotingApp from "./components/VotingApp";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
+   redirect("/maintenance");
    const user = await getCurrentUser();
 
    return (
