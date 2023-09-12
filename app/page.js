4954dafc4a9e4d3f36062ead214df25ef8ff227f
Adam Romero <adamromero89@gmail.com>
2023-09-11 17:15:12 -0700

    Add console logs to page

--- app/page.js
+++ app/page.js
@@ -23,6 +23,9 @@ export default async function Home() {
    const isUnderRequestLimit =
       currentUsersMonthlyRequests.length < requestLimit;
 
+   console.log("current user requests month: ", currentUsersMonthlyRequests);
+   console.log("is under limit: ", isUnderRequestLimit);
+
    return (
       <main>
          <Header />
