5e8feb902fe13f86a8dd9b6123a84d0c9d691fbd
Adam Romero <adamromero89@gmail.com>
2024-01-07 21:48:30 -0800

    Add parent container for poster image to avoid layout shift

--- app/components/MovieListEntry.jsx
+++ app/components/MovieListEntry.jsx
@@ -353,21 +353,24 @@ const MovieListEntry = ({
                      </span>
                   </h2>
                   <div className="flex gap-[20px] flex-col xs:flex-row">
-                     {data?.data?.Poster ? (
-                        <img
-                           src={
-                              data?.data?.Poster.includes("media-amazon")
-                                 ? data?.data?.Poster
-                                 : `https://image.tmdb.org/t/p/w300_and_h450_bestv2${data?.data?.Poster}`
-                           }
-                           alt={data?.data?.Title}
-                           className="h-[200px] sm:h-[275px] mx-auto"
-                        />
-                     ) : (
-                        <div className="flex justify-center items-center bg-[#585858] h-[200px] w-[135px] sm:h-[275px] sm:w-[183px] mx-auto">
-                           <FaRegImage className="text-[40px]" />
-                        </div>
-                     )}
+                     <div className="w-[135px] sm:w-[185px]">
+                        {data?.data?.Poster ? (
+                           <img
+                              src={
+                                 data?.data?.Poster.includes("media-amazon")
+                                    ? data?.data?.Poster
+                                    : `https://image.tmdb.org/t/p/w300_and_h450_bestv2${data?.data?.Poster}`
+                              }
+                              alt={data?.data?.Title}
+                              className="h-[200px] sm:h-[275px] mx-auto"
+                           />
+                        ) : (
+                           <div className="flex justify-center items-center bg-[#585858] h-[200px] w-[135px] sm:h-[275px] sm:w-[185px] mx-auto">
+                              <FaRegImage className="text-[40px]" />
+                           </div>
+                        )}
+                     </div>
+
                      <div className="flex flex-1 flex-col">
                         {data?.data?.Genre && (
                            <div>
