9bdbe6cdd83b7d9f469e5097586c72e7ebc2abfd
Adam Romero <adamromero89@gmail.com>
2024-08-01 19:57:59 -0700

    Fetch data on test page

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import { getCurrentUser } from "@/lib/session";
 
 const TestPage = async () => {
-   const user = await getCurrentUser();
-   if (!user) redirect("/");
+   const movieResponse = await fetch(`${process.env.API_URL}/api/movies`);
+   const movieRequests = await movieResponse.json();
 
    return (
-      <div className="pt-[50px] px-[20px] leading-[35px]">
-         <div className="text-[25px] flex items-center justify-center">
-            Hi {user.name}. You are a{" "}
-            {user.isProducer ? "Producer" : "non-producer patron"} and should
-            have {user.isProducer ? "3" : "2"} new requests per month.
-         </div>
+      <div>
+         {movieRequests.map((request, index) => (
+            <div key={request._id}>
+               {index + 1}: {request.data.Title}
+            </div>
+         ))}
       </div>
    );
 };
