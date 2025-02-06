ff29394dbb2fa61c772324c3b09eebcb723eb51d
Adam Romero <adamromero89@gmail.com>
2025-02-06 14:28:56 -0800

    Minor adjustments for month date formatting

--- app/utils/convertDateFormat.js
+++ app/utils/convertDateFormat.js
@@ -1,35 +1,17 @@
-const months = [
-   "January",
-   "February",
-   "March",
-   "April",
-   "May",
-   "June",
-   "July",
-   "August",
-   "September",
-   "October",
-   "November",
-   "December",
-];
-
-export const convertDateFormat = (date, releaseDate) => {
-   let originalDate, month, day, year;
-   if (releaseDate) {
-      month = date?.split("-")[1];
-      day = date?.split("-")[2];
-      year = date?.split("-")[0];
-      originalDate = new Date(`${month}-${day}-${year}`);
-   }
-
-   originalDate = new Date(date);
-   month = originalDate.getMonth();
-   day = originalDate.getDate();
-   year = originalDate.getFullYear();
-
-   return `${months[month]} ${day}, ${year}`;
+export const convertReleaseDate = (date) => {
+   const month = date?.split("-")[1];
+   const day = date?.split("-")[2];
+   const year = date?.split("-")[0];
+   return `${month}-${day}-${year}`;
 };
 
-export const convertMonthFormat = (month) => {
-   return `${months[month]}`;
+export const convertDateFormat = (date) => {
+   const originalDate = new Date(date);
+
+   const month = originalDate.toLocaleString(undefined, { month: "long" });
+   const day = originalDate.getDate();
+   const year = originalDate.getFullYear();
+   //const time = originalDate.toLocaleTimeString();
+
+   return `${month} ${day}, ${year}`;
 };
