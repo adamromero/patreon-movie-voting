12167b9b28aa24410cf093e34f3523025036768f
Adam Romero <adamromero89@gmail.com>
2025-08-13 15:47:37 -0700

    Rewrite mongodb file for typescript

--- lib/connectDB.ts
+++ lib/connectDB.ts
@@ -1,41 +1,21 @@
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 
-const MONGO_URI = process.env.MONGODB_URI;
+const MONGO_URI = process.env.MONGODB_URI as string;
+if (!MONGO_URI) throw new Error("Please add your MONGO_URI to .env.local");
 
-if (!MONGO_URI) {
-   throw new Error("Please add your MONGO_URI to .env.local");
-}
+let cached: { conn: Mongoose | null; promise: Promise<Mongoose> | null } = (
+   global as any
+).mongooseCache || { conn: null, promise: null };
 
-let globalWithMongoose = global;
-let cached = globalWithMongoose.mongoose;
+(global as any).mongooseCache = cached;
 
-if (!cached) {
-   cached = globalWithMongoose.mongoose = { conn: null, promise: null };
-}
-
-async function connectDB() {
-   if (cached.conn) {
-      return cached.conn;
-   }
+export default async function connectDB(): Promise<Mongoose> {
+   if (cached.conn) return cached.conn;
 
    if (!cached.promise) {
-      cached.promise = mongoose
-         .connect(MONGO_URI)
-         .then((mongoose) => {
-            return mongoose;
-         })
-         .catch((error) => {
-            console.error("MongoDB connection error:", error);
-            throw error;
-         });
+      cached.promise = mongoose.connect(MONGO_URI).then((m) => m);
    }
 
-   try {
-      cached.conn = await cached.promise;
-      return cached.conn;
-   } catch (error) {
-      throw error;
-   }
+   cached.conn = await cached.promise;
+   return cached.conn;
 }
-
-export default connectDB;
