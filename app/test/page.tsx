f70b45cb960e23e965862d2fe2ae138471835bae
Adam Romero <adamromero89@gmail.com>
2025-08-13 15:36:43 -0700

    Remove fetch call on test page

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,16 +1,9 @@
 import React from "react";
 
 const TestPage = async () => {
-   const movieResponse = await fetch(`${process.env.API_URL}/api/movies`);
-   const movieRequests = await movieResponse.json();
-
    return (
       <div>
-         {movieRequests.map((request, index) => (
-            <div key={request._id}>
-               {index + 1}: {request.data.Title}
-            </div>
-         ))}
+         <h1>This is only a test page</h1>
       </div>
    );
 };
