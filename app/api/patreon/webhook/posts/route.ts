99921006c7717a9a05f5609b9bc4facbaba25994
Adam Romero <adamromero89@gmail.com>
2026-05-09 17:23:34 -0700

    Create posts folder and move request check off route inside

--- app/api/patreon/webhook/posts/route.ts
+++ app/api/patreon/webhook/posts/route.ts
@@ -5,6 +5,7 @@ export async function POST(req: NextRequest) {
    try {
       const response = await req.json();
       const result = await setOnChannel(response);
+      return NextResponse.json(result);
    } catch (error) {
       return NextResponse.json({
          error: "Unable to update request status to on channel in the database.",
