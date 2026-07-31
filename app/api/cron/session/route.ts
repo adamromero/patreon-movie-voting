22cb90b84c6d726c4c11f67feb4aef4c05ab2a8a
adamromero <adamromero89@gmail.com>
2026-07-30 17:17:23 -0700

    Add console log to cron route

--- app/api/cron/session/route.ts
+++ app/api/cron/session/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
    try {
+      console.log("Running cron...");
       const result = await sessionCleanup();
       return NextResponse.json(result);
    } catch (error) {
